b62da245c4d90940b6347e39aed23a6c7a0d75c9
pedrosamegima <phsamegima@gmail.com>
2024-04-30 16:09:59 -0300

    Facebook

---
+++ src/app/index.js
@@ -0,0 +1,35 @@
+import { View, Text, Image } from 'react-native';
+import { styles } from '../css/style';
+import Botao from '../components/botao';
+import InputTexto from '../components/inputTexto';
+import { useState } from 'react';
+
+export default function App() {
+  const [valida, setValida] = useState();
+  const atualizaTexto = (newText) =>{
+    console.log(newText);
+  }
+  return (
+  
+    <View style={styles.container}>
+    <Image
+    style={{width:'80%', height:80, marginBottom:30}}
+    resizeMode="contain"
+    source={require('../../assets/facebook.png')}
+      />
+
+      <InputTexto label="Digite seu numero ou email" dica="Digite seu numero ou email"/>
+      <InputTexto label="Digite sua senha" dica="Digite sua senha" seguranca={true}/>
+      
+ 
+
+      <Botao btn="entrar" cor='#1477F2' src='home' />
+      <View style={styles.cont}>
+        <View style={styles.line}/>
+        <Text style={styles.text}>ou</Text>
+        <View style={styles.line}/>
+        </View>
+      <Botao btn="criar uma nova conta" cor='' src='criar uma nova conta' />
+    </View>
+  );
+}
---
+++ src/css/botaoStyle.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     borderWidth: 1,
+     borderColor: '#000',
+     paddingVertical: 20,
+     paddingHorizontal: 40,
+     borderRadius: 5,
+     marginVertical: 10,
+ 
+   
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        color: '#010101',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    btn1:{
+        color: '#f0f0f0',
+    }
+  });
+  
